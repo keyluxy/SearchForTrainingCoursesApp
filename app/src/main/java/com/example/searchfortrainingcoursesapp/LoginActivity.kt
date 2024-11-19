9598350465a6cd6f6ddf0c90feb8197099f4f0fb
Amir <amir080121@gmail.com>
2024-11-19 12:15:05 +0300

    add: login activity, registration activity

--- app/src/main/java/com/example/searchfortrainingcoursesapp/LoginActivity.kt
+++ app/src/main/java/com/example/searchfortrainingcoursesapp/LoginActivity.kt
@@ -1,20 +1,45 @@
 package com.example.searchfortrainingcoursesapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.searchfortrainingcoursesapp.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+//        setContentView(R.layout.activity_login)
+
+        val email = binding.emailEt.text.toString()
+        val password = binding.passwordEt.text.toString()
+
+        binding.loginBtn.setOnClickListener {
+            if (email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(application, "Fields cannot be empty", Toast.LENGTH_SHORT).show()
+            } else {
+                FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            startActivity(Intent(this, MainActivity::class.java))
+                        }
+                    }
+            }
         }
+
+        binding.goToRegistrActivityTv.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
+
     }
 }
--- app/src/main/java/com/example/searchfortrainingcoursesapp/RegisterActivity.kt
+++ app/src/main/java/com/example/searchfortrainingcoursesapp/RegisterActivity.kt
@@ -1,20 +1,63 @@
 package com.example.searchfortrainingcoursesapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.searchfortrainingcoursesapp.databinding.ActivityLoginBinding
+import com.example.searchfortrainingcoursesapp.databinding.ActivityRegisterBinding
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_register)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+//        setContentView(R.layout.activity_register)
+
+
+
+
+        binding.registrBtn.setOnClickListener {
+            if (binding.emailRegEt.text.toString().isEmpty() || binding.passwordRegEt.text.toString().isEmpty()
+                || binding.rememberPassRegEt.text.toString().isEmpty()) {
+                Toast.makeText(application, "Fields cannot be empty", Toast.LENGTH_SHORT).show()
+            } else {
+                FirebaseAuth.getInstance().createUserWithEmailAndPassword(binding.emailRegEt.text.toString(), binding.passwordRegEt.text.toString())
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+
+                            val userInfo: HashMap<String, String> = HashMap()
+                            userInfo["email"] = binding.emailRegEt.text.toString()
+                            userInfo["password"] = binding.passwordRegEt.text.toString()
+                            userInfo["package_name"] = packageName
+                            FirebaseAuth.getInstance().currentUser?.let {
+                                FirebaseDatabase.getInstance().getReference().child("Users").child(
+                                    it.uid).setValue(userInfo)
+                            }
+
+                            startActivity(Intent(this, MainActivity::class.java))
+
+                        } else {
+                            Toast.makeText(application, "Registration failed: ${task.exception?.message}", Toast.LENGTH_SHORT).show()
+
+                        }
+                    }
+
+            }
+        }
+
+        binding.goToLoginActivity.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
         }
     }
 }
